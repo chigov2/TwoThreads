0efa7764f3b82ae454fc59e003930529d6f1c59b
chigov2 <chigov@gmail.com>
2021-11-08 16:31:27 +0200

    Initial commit

---
+++ app/src/main/java/techmarket/uno/thread/thread/Main.java
@@ -0,0 +1,43 @@
+package techmarket.uno.thread.thread;
+
+public class Main {
+    static boolean isFive = false;
+
+    public static void main(String[] args) {
+
+        Thread timer = new Thread(new Runnable() {
+            @Override//timer.start();
+            public void run() {
+                try {
+                    int i = 0;
+                    while (true) {
+                        if (i == 5){
+                            isFive = true;
+                        }
+                        System.out.println(i);
+                        Thread.sleep(1000);
+                        i++;
+                    }
+                } catch (Exception e) {
+                }
+            }
+        });
+        timer.start();
+
+        Thread timer2 = new Thread(new Runnable() {
+            @Override//timer.start();
+            public void run() {
+                try {
+                    int i = 0;
+                    while (!isFive) {
+                        System.out.println(i);
+                        Thread.sleep(1000);
+                        i++;
+                    }
+                } catch (Exception e) {
+                }
+            }
+        });
+        timer2.start();
+    }
+}
---
+++ app/src/main/java/techmarket/uno/thread/thread/TwoThreads.java
@@ -0,0 +1,4 @@
+package techmarket.uno.thread.thread;
+
+public class TwoThreads {
+}
